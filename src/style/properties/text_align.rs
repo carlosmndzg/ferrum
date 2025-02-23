d52a66eed8a0bae902d59c41bcbef563f9525d15
carlosmndzg <cjr2344@gmail.com>
2025-02-23 18:40:08 +0100

    Implemented justify alignment

--- src/style/properties/text_align.rs
+++ src/style/properties/text_align.rs
@@ -1,10 +1,14 @@
-use crate::{css::types::Value, layout::types::LayoutNode};
+use crate::{
+    css::types::Value,
+    layout::types::{BoxType, LayoutNode},
+};
 
 #[derive(Debug, Clone, PartialEq)]
 pub(crate) enum TextAlign {
     Left,
     Right,
     Center,
+    Justify,
 }
 
 impl TextAlign {
@@ -14,6 +18,7 @@ impl TextAlign {
                 "left" => return Some(TextAlign::Left),
                 "right" => return Some(TextAlign::Right),
                 "center" => return Some(TextAlign::Center),
+                "justify" => return Some(TextAlign::Justify),
                 _ => {}
             }
         }
@@ -34,21 +39,78 @@ impl TextAlign {
     }
 
     pub(crate) fn apply(&self, node: &mut LayoutNode) {
+        match self {
+            TextAlign::Left => {}
+            TextAlign::Center => self.apply_text_align_center(node),
+            TextAlign::Right => self.apply_text_align_right(node),
+            TextAlign::Justify => self.apply_text_align_justify(node),
+        }
+    }
+
+    fn apply_text_align_center(&self, node: &mut LayoutNode) {
         for line in &mut node.children {
             let line_width = line.box_dimensions.content.width;
             let remaining_space = node.box_dimensions.content.width - line_width;
 
             for word in &mut line.children {
-                match self {
-                    TextAlign::Center => {
-                        word.box_dimensions.content.x += remaining_space / 2.0;
-                    }
-                    TextAlign::Right => {
-                        word.box_dimensions.content.x += remaining_space;
-                    }
-                    _ => {}
+                word.box_dimensions.content.x += remaining_space / 2.0;
+            }
+        }
+    }
+
+    fn apply_text_align_right(&self, node: &mut LayoutNode) {
+        for line in &mut node.children {
+            let line_width = line.box_dimensions.content.width;
+            let remaining_space = node.box_dimensions.content.width - line_width;
+
+            for word in &mut line.children {
+                word.box_dimensions.content.x += remaining_space;
+            }
+        }
+    }
+
+    fn apply_text_align_justify(&self, node: &mut LayoutNode) {
+        let length = node.children.len();
+        let lines_except_last = &mut node.children[..length - 1];
+
+        for line in lines_except_last {
+            let line_width = line.box_dimensions.content.width;
+            let remaining_space =
+                node.box_dimensions.content.width + self.count_whitespace_length(line) - line_width;
+
+            line.children.retain(|word| {
+                if let BoxType::Word(word) = &word.box_type {
+                    word.text != " "
+                } else {
+                    false
+                }
+            });
+
+            if line.children.len() <= 1 {
+                continue;
+            }
+
+            let space = remaining_space / (line.children.len() - 1) as f32;
+            let mut acc_x = 0.;
+
+            for word in &mut line.children {
+                word.box_dimensions.content.x = line.box_dimensions.content.x + acc_x;
+                acc_x += word.box_dimensions.content.width + space;
+            }
+        }
+    }
+
+    fn count_whitespace_length(&self, line: &LayoutNode) -> f32 {
+        let mut count = 0.0;
+
+        for word in &line.children {
+            if let BoxType::Word(word_box) = &word.box_type {
+                if word_box.text == " " {
+                    count += word.box_dimensions.content.width;
                 }
             }
         }
+
+        count
     }
 }
